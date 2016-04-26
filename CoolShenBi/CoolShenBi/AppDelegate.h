6f79bdeae4f59d0b065f34a29cde5a85170b79cc
gaoning1990 <gaoning199007@163.com>
2016-04-26 15:51:22 +0800

    Add New Code

---
+++ CoolShenBi/CoolShenBi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoolShenBi
+//
+//  Created by GN on 16/4/26.
+//  Copyright © 2016年 NewChinese. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoolShenBi/CoolShenBi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoolShenBi
+//
+//  Created by GN on 16/4/26.
+//  Copyright © 2016年 NewChinese. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
